caff67e4828150656e41d1dddce4bf93ede7ab1e
KC Erb <kc@kcerb.com>
2021-02-06 17:17:09 -0500

    cosmetic change to index.js

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ import optimizers from './src/optimizers/index.js';
 import drivers from './src/drivers/index.js';
 
 export default Aligner;
-export { optimizers, drivers};
+export { optimizers, drivers };
